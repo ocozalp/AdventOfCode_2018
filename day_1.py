712342000a41cb401533724446e85d4549d6b0cb
ocozalp <ozalp.orhan@gmail.com>
2018-12-06 13:08:18 -0500

    Day 1 - Case 2

--- day_1.py
+++ day_1.py
@@ -3,9 +3,26 @@ def main():
         numbers = list(map(int, [s[:-1] for s in f.readlines()]))
 
     print('Case 1: %d' % solve_case_1(numbers))
+    print('Case 2: %d' % solve_case_2(numbers))
 
 def solve_case_1(numbers):
     return sum(numbers)
     
+def solve_case_2(numbers):
+    seen = {0}
+    result = None
+    current = 0
+
+    while result is None: 
+        for number in numbers:
+            current += number
+            if current in seen:
+                result = current
+                break
+
+            seen.add(current)   
+
+    return result
+
 if __name__ == '__main__':
     main()
